58d54418c05e875f6502640b3e0db345735cc0bf
nanin <naninaga196@gmail.com>
2024-06-27 10:39:35 +0530

    demo

---
+++ src/employe/EmployeDemo.java
@@ -0,0 +1,36 @@
+package employe;
+
+public class EmployeDemo {
+    public String name;
+    public int empnewId;
+    public long number;
+
+    public EmployeDemo(String name, int empnewId, long number) {
+        this.name = name;
+        this.empnewId = empnewId;
+        this.number = number;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "EmployeDemo{" +
+                "name='" + name + '\'' +
+                ", empnewId=" + empnewId +
+                ", number=" + number +
+                '}';
+    }
+
+}
+
+
+
+
+//    @Override
+//    public String toString() {
+//        return "Employe [id=" + empnewId + ",name=" + ", number=" + number;
+//    }
+
+
+
---
+++ src/employe/EmployeTest.java
@@ -0,0 +1,13 @@
+package employe;
+
+
+public class EmployeTest {
+
+    public static void main(String[] args) {
+        //Auto generate to empid
+       EmployeDemo empid=new EmployeDemo("neoteric",12568,148452155);
+        System.out.println(empid.empnewId);
+
+
+    }
+}
